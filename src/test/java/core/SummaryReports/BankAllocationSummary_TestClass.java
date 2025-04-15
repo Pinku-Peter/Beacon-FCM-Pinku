6fc2b56af407c11904c4ee4f1a9e4ec98a2ac6f9
Pinku-Peter <pinku.peter@speridian.com>
2025-04-15 10:25:33 +0530

    41th Commit.

--- src/test/java/core/SummaryReports/BankAllocationSummary_TestClass.java
+++ src/test/java/core/SummaryReports/BankAllocationSummary_TestClass.java
@@ -33,7 +33,7 @@ import com.listeners.TestListener;
 public class BankAllocationSummary_TestClass {
 	
 	Base_Class baseclass;
-	com.Utility.ExcelReader ExcelReader;
+	static com.Utility.ExcelReader ExcelReader;
 	WebDriver driver;
 	private List<WebDriver> drivers = new ArrayList<>();
 	TestListener TestListener;
@@ -63,9 +63,9 @@ public class BankAllocationSummary_TestClass {
     }
 	
 	@Test(priority = 1, dataProvider = "TestData")
-    public void testSuccessfulAccountAllocation(Map<Object, Object> testdata) throws Exception {
-		try {
-	// Login into the core application
+    public void Verify_zone_user_able_to_allocate_accounts_to_branch(Map<Object, Object> testdata) throws Exception { 
+		try { 
+	// Login into the core application  
 	 Login_Class.CoreLogin(); 
 	 driver = baseclass.getDriver();
 	 bankallocationsummary = new BankAllocationSummary_MainClass(driver);
@@ -80,11 +80,22 @@ public class BankAllocationSummary_TestClass {
 	        String value5 = testdata.get("OsBalanceOperator").toString(); 
 	        String value6 = testdata.get("OsBalanceAmount").toString();
 	        String value7 = testdata.get("AllocateTo").toString();
+	        
+	        List<Object> results = bankallocationsummary.createBranchUser(value4);  
+			 
+	        // You can use the results if needed
+		 userid = (String) results.get(0);
+	     password = (String) results.get(1);
+	     String statusMessage = (String) results.get(2);
+	     ExtentTestManager.getTest().log(Status.PASS, "The stored procedure 'SP_PICK_EXISTING_CORE_BRANCH_USER' was executed successfully to get a branch user."+statusMessage);
+	     
+	     List<Object> status = bankallocationsummary.deleteZoneUserToBranchUserAccountAllocation(value4,value7,userid);
+	     ExtentTestManager.getTest().log(Status.PASS, "Status: " + status.toString());
+	     
+	     
+	     List<Object> status2 = bankallocationsummary.insertBCOUser(userid);   
+	     ExtentTestManager.getTest().log(Status.PASS, "Status: " + status2.toString());
 	 
-	 List<Object> results1 = bankallocationsummary.deleteZoneUserToBranchUserAccountAllocation(value4,value7,"IBU0000240");
-	 ExtentTestManager.getTest().log(Status.PASS, String.valueOf(results1.get(0)));
-	 List<Object> results2 = bankallocationsummary.insertBCOUser("IBU0000240");
-	 ExtentTestManager.getTest().log(Status.PASS, String.valueOf(results2.get(0)));
 	        bankallocationsummary.selectSmaTile();
 	        ExtentTestManager.getTest().log(Status.PASS, "Navigated to My Desk.");
 	        ExtentTestManager.getTest().log(Status.PASS, "Selected the SMA tile from Unassigned Accounts.");
@@ -117,66 +128,84 @@ public class BankAllocationSummary_TestClass {
     }
 	
 	 @Test(priority = 2) 
-	    public void testValidUserLogin() throws Exception{
-		 
-//		 List<Object> results = bankallocationsummary.createBranchUser();  
-//
-//	        // You can use the results if needed
-//		 userid = (String) results.get(0);
-//	     password = (String) results.get(1);
-//	     String statusMessage = (String) results.get(2);
-//	     System.out.println(statusMessage);  
-		 Login_Class.CoreLoginWithInputs("IBU0000240","ses@987"); 
+	    public void Verify_branch_user_able_to_Login_with_Valid_Credentials() throws Exception{  
+		 try {
+		 Login_Class.CoreLoginWithInputs(userid,password);  
 		 driver = baseclass.getDriver();
 		 drivers.add(driver);
 		 bankallocationsummary = new BankAllocationSummary_MainClass(driver);
 		 screenShot = new com.Utility.ScreenShot(driver);
+		 ExtentTestManager.getTest().log(Status.PASS, "Successfully logged in using valid branch user credentials.");
 		 WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(180));
 		// Assertion for successful login
 	        String expectedURL = "http://192.168.32.33:8599/Home";
 	        Assert.assertEquals( driver.getCurrentUrl(),expectedURL);
+	        ExtentTestManager.getTest().log(Status.PASS, "Successfully logged in, and the user was navigated to the dashboard.");
 	        WebElement username = wait.until(ExpectedConditions.visibilityOfElementLocated(CoreCollectionOfficerConfigRepo.username));
 			 userName = username.getText(); 
 			 WebElement userid = wait.until(ExpectedConditions.visibilityOfElementLocated(UpdationofDispositionRepo.loginuserid));
 			 userId = userid.getText();
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000);
 	    }
 	 
 	 @Test(priority = 3)
-	    public void testAccountAllocation() throws InterruptedException {
-
+	    public void Successful_Account_Allocation_to_My_accounts_in_my_desk() throws InterruptedException { 
+		 try {
 		 bankallocationsummary.navigateToMyDesk();
+		 ExtentTestManager.getTest().log(Status.PASS, "Navigated to my desk section.");
 		 bankallocationsummary.selectSmaFromBranchAttention();
-		 bankallocationsummary.selectAllAccountsFromFirstPage();
+		 ExtentTestManager.getTest().log(Status.PASS, "Selected SMA from the 'Branch Attention Required' section.");
+		 bankallocationsummary.selectAllAccountsFromFirstPage(); 
+		 ExtentTestManager.getTest().log(Status.PASS, "Selected all accounts from the first page.");
 		 account_numbers_count=bankallocationsummary.getAccountNumbersFromGrid();
 		 bankallocationsummary.assignToBranch(userName);
+		 ExtentTestManager.getTest().log(Status.PASS, "Chose 'Branch' from the 'Assign/Re-assign to' dropdown.");
 		 bankallocationsummary.clickAssign();
+		 ExtentTestManager.getTest().log(Status.PASS, "Clicked on the 'Assign' button.");
 	        Assert.assertTrue(bankallocationsummary.isConfirmationDisplayed(), "Accounts not Assigned successfully.");
-	        
+	        ExtentTestManager.getTest().log(Status.PASS, "Accounts were Assigned successfully, and a confirmation message was displayed."); 
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000);
 	    }
 	 
 	 @Test(priority = 4) 
-	    public void validateDataExecutionForBankAllocationSummary() throws IOException {
-	       
+	    public void Validate_Data_Execution_for_Bank_Allocation_Summary_in_database() throws IOException, InterruptedException { 
+		 try {
 		 String statusMessage = bankallocationsummary.insertAccountsForUser(userId); 
-
-		    System.out.println("Status message: " + statusMessage);
+		 ExtentTestManager.getTest().log(Status.PASS, statusMessage); 
 
 		    String expectedMessage = "Accounts inserted into TRN_AC_MOVEMENT with  MOVEMENT TYPE=1 and MOVEMENT_TYPE_CATEGORY=NEW";
 		    
 		    Assert.assertTrue(statusMessage.equalsIgnoreCase(expectedMessage),
-		            "Test failed - Expected message: '" + expectedMessage + "', but got: '" + statusMessage + "'");
+		            "Test failed - Expected message: '" + expectedMessage + "', but got: '" + expectedMessage + "'");
+		    ExtentTestManager.getTest().log(Status.PASS, "Accounts were successfully inserted into the TRN_AC_MOVEMENT table with MOVEMENT_TYPE = '1' and MOVEMENT_TYPE_CATEGORY = 'NEW'.");
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000);
 	    }
 	 
 	 @Test(priority = 5, dataProvider = "TestData")
-	    public void testVerifyTodaysAccountsInBranchAllocation(Map<Object, Object> testdata) throws Exception { 
-		 
+	    public void Verify_Accounts_in_Todays_Branch_Allocation_Summary(Map<Object, Object> testdata) throws Exception {  
+		 try {
 		 Login_Class.CoreLogin(); 
 		 driver = baseclass.getDriver();
 		 bankallocationsummary = new BankAllocationSummary_MainClass(driver);
 		 screenShot = new com.Utility.ScreenShot(driver);
 		 
 		 bankallocationsummary.openAllocationSummaryWindow(); 
-		 
+		 ExtentTestManager.getTest().log(Status.PASS, "Opened the Branch Allocation Summary window.");
 		 if (testdata.get("Run").toString().equalsIgnoreCase("Yes")) {
 		    	
 		        String value1 = testdata.get("Region").toString();
@@ -191,46 +220,66 @@ public class BankAllocationSummary_TestClass {
 	        
 	        // Verifying that today's allocations are displayed correctly
 	        Assert.assertTrue(bankallocationsummary.areTodaysAllocationsDisplayed(account_numbers_count), "Today's allocated accounts are not displayed correctly.");
+	        ExtentTestManager.getTest().log(Status.PASS, "Selected the branch and verified that today's allocated accounts were listed.");
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000); 
 	    }  
 	 
 	 @Test(priority = 6)
-	    public void testDownloadFunctionality() {
-
+	    public void Verify_Download_Functionality_as_Excel() throws InterruptedException { 
+		 try {
 		 bankallocationsummary.downloadAsExcel(); 
-	        
+		 ExtentTestManager.getTest().log(Status.PASS, "Clicked on 'Download as Excel'");  
 	     Assert.assertTrue(bankallocationsummary.isExcelvaluesMatchingWithUI(), "Excel file is not matching with UI displayed values");
-
+	     ExtentTestManager.getTest().log(Status.PASS, "Opened the downloaded Excel file and found the details as per in grid.");
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000); 
 	    } 
 	 
 	 @Test(priority = 7, dataProvider = "TestData")
-	    public void testVerifyAccountsInYesterdaysDate(Map<Object, Object> testdata) throws IOException {
-		 
-		 if (testdata.get("Run").toString().equalsIgnoreCase("Yes")) {
+	    public void Verify_Accounts_in_Yesterdays_date(Map<Object, Object> testdata) throws IOException, InterruptedException { 
+		 try {
+		 if (testdata.get("Run").toString().equalsIgnoreCase("Yes")) { 
 		    	
 		        String value1 = testdata.get("AllocateTo").toString();
 		        String value2 = testdata.get("Branch").toString();
 	        
-		        String statusMessage = bankallocationsummary.updateAllocatedDate(value2,value1,userId); 
+		        String statusMessage = bankallocationsummary.updateAllocatedDate(value2,value1,userId);  
 		        
-		        System.out.println(statusMessage);	
+		        ExtentTestManager.getTest().log(Status.PASS, statusMessage);	
 		        
 		        Assert.assertTrue(statusMessage.equalsIgnoreCase("Update successful."), 
 		                "Test failed - Expected message 'Update successful.' but got: " + statusMessage); 
+		        ExtentTestManager.getTest().log(Status.PASS, statusMessage);
 		        
-		        String statusMessage2 = bankallocationsummary.runBranchAllocDashboard(); 
-		        
-		        System.out.println(statusMessage2);		        
+		        String statusMessage2 = bankallocationsummary.runBranchAllocDashboard();  	        
 		        
 		        Assert.assertTrue(statusMessage2.equalsIgnoreCase("Success"), 
 		                "Test failed - Expected message 'Success' but got: " + statusMessage2);
+		        ExtentTestManager.getTest().log(Status.PASS, statusMessage2);      
 		 }
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000);
 		 
 	    }
 	 
 	 @Test(priority = 8)
-	    public void verifyTablesUpdatedAfterPackageExecution() throws IOException {
+	    public void Verify_the_tables_DW_BRANCH_ALLOCATIONS_SUMMARY_and_DW_BRANCH_ALLOCATIONS_DASHBOARD_are_updated_after_the_package_execution() throws IOException, InterruptedException {
+		 try {
 	        // Step 1: Execute the stored procedure
-		 List<Object> statusMessage = bankallocationsummary.checkDashboardDataLoadExecution(userId);
+		 List<Object> statusMessage = bankallocationsummary.checkDashboardDataLoadExecution(userId);  
 
 	        // Step 2: Check the outputs
 	        String resultSummary = (String) statusMessage.get(0);
@@ -238,26 +287,41 @@ public class BankAllocationSummary_TestClass {
 
 	        // Expected Results
 	        Assert.assertEquals(resultSummary, "Yes", "DW_BRANCH_ALLOCATIONS_SUMMARY table entry check failed");
+	        ExtentTestManager.getTest().log(Status.PASS, "DW_BRANCH_ALLOCATIONS_SUMMARY table entry : " +resultSummary);
 	        Assert.assertEquals(resultDashboard, "Yes", "DW_BRANCH_ALLOCATIONS_DASHBOARD table entry check failed");
+	        ExtentTestManager.getTest().log(Status.PASS, "DW_BRANCH_ALLOCATIONS_DASHBOARD table entry : " +resultDashboard);
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000);
 	    }
 	 
 	 
 	 
 	 @Test(priority = 9, dataProvider = "TestData")
-	    public void verifyGridDetailsAccuracy(Map<Object, Object> testdata ) {
-		 
+	    public void Verify_Grid_Details_Accuracy(Map<Object, Object> testdata ) throws InterruptedException {  
+		 try {
 		 if (testdata.get("Run").toString().equalsIgnoreCase("Yes")) {
 		    	
 		        String value1 = testdata.get("YesterdaysDate").toString(); 
 		        String value2 = testdata.get("YesterdaysDate").toString();
 		        
 		        bankallocationsummary.selectyesterdaysdate(value1, value2);   
-		        
+		        ExtentTestManager.getTest().log(Status.PASS, "Selected date from date picker : "+value1);  
 		 }
 		 bankallocationsummary.clickSearchButton();
-	        
+		 ExtentTestManager.getTest().log(Status.PASS, "Clicked on Search button.");
 	        // Verifying that today's allocations are displayed correctly
 	        Assert.assertTrue(bankallocationsummary.areYesterdaysAllocationsDisplayed(account_numbers_count), "Yesterday's's allocated accounts are not displayed correctly.");
+	        ExtentTestManager.getTest().log(Status.PASS, "Accounts were displayed properly");
+		 }
+			catch (AssertionError | Exception e) {
+				ExtentTestManager.getTest().log(Status.FAIL, "Test Failed: " + e.getMessage());
+	           throw e;
+		 }
+			Thread.sleep(3000);
 	    }
 	 
 	@AfterMethod 
@@ -281,7 +345,7 @@ public class BankAllocationSummary_TestClass {
 	
 	 
 	 @DataProvider(name = "TestData")
-		public Object[][] gettestdate() throws IOException {
+		public static Object[][] gettestdate() throws IOException {
 		 ExcelReader = new com.Utility.ExcelReader("CoreBankAllocationSummary");
 			Object[][] objectarry = null;
 			java.util.List<Map<String, String>> completedata = com.Utility.ExcelReader.getdata();
@@ -298,16 +362,16 @@ public class BankAllocationSummary_TestClass {
 	 public void afterEachTest() {
 	     ExtentManager.getInstance().flush();
 	  // Close all tracked browser instances
-//	        for (WebDriver driverInstance : drivers) {
-//	            if (driverInstance != null) {
-//	                driverInstance.quit();
-//	            }
-//	        }
-//
-//	        // Clear the list of drivers
-//	        drivers.clear();
-//
-//	        System.out.println("All browser instances have been closed.");
+	        for (WebDriver driverInstance : drivers) {
+	            if (driverInstance != null) {
+	                driverInstance.quit();
+	            }
+	        }
+
+	        // Clear the list of drivers
+	        drivers.clear();
+
+	        System.out.println("All browser instances have been closed.");
 	    }
 
 
